754957405908eac0033c8110b366f8b656e1563b
Nam Pham <pvnam95@hotmail.com>
2024-10-27 12:36:05 +0900

    simplify flow

--- src/WebApp/src/pages/settings/index.tsx
+++ src/WebApp/src/pages/settings/index.tsx
@@ -1,22 +1,7 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect, useState } from "react";
 const CLIENT_ID = import.meta.env.VITE_GITHUB_CLIENTID;
 const REDIRECT_URI = import.meta.env.VITE_GITHUB_REDIRECT_URI;
 
 export default function Settings() {
-  const { getAccessTokenSilently } = useAuth0();
-  const [githubConnected, setGitHubConnected] = useState(false);
-  const onClickConnectGitHub = async () => {
-    if (!githubConnected) {
-      //repo and read:org scope
-      const scope = "repo%20read:org";
-      const authUrl = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${scope}`;
-      window.location.href = authUrl;
-    } else {
-      //todo: disconnect
-    }
-  };
-
   const onReconnectGitHub = async () => {
     //repo and read:org scope
     const scope = "repo%20read:org";
@@ -24,39 +9,16 @@ export default function Settings() {
     window.location.href = authUrl;
   };
 
-  useEffect(() => {
-    (async () => {
-      const accessToken = await getAccessTokenSilently();
-      const res = await fetch(`/api/me`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      if (res.ok) {
-        const { connections } = await res.json();
-        setGitHubConnected(
-          connections.find((c: any) => c.provider === "GitHub"),
-        );
-      }
-    })();
-  }, []);
-
   return (
     <div>
       <h1 className="font-bold">Settings</h1>
       <section className="mt-4">
         <h2>Connections</h2>
         <button
-          onClick={onClickConnectGitHub}
-          className="rounded-sm border bg-gray-900 px-2 py-1.5 text-white"
-        >
-          {githubConnected ? "Disconnect" : "Connect"} GitHub
-        </button>
-        <button
           onClick={onReconnectGitHub}
           className="rounded-sm border bg-gray-900 px-2 py-1.5 text-white"
         >
-          Reconnect GitHub
+          Connect GitHub
         </button>
       </section>
     </div>
