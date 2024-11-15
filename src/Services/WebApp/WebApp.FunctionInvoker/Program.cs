f454d82c112a8743ccbaf53285ddf30d7507d5d9
Nam Pham <pvnam95@hotmail.com>
2024-11-15 09:09:45 +0900

    Add Dockerfile and program logic for WebApp.FunctionInvoker

---
+++ src/Services/WebApp/WebApp.FunctionInvoker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using Jint;
+
+Result result = new();
+var engine = new Engine();
+
+var handler = File.ReadAllText("handler.js");
+engine.Execute(handler);
+
+var jsResult = engine.Evaluate("(() => { return handler(request); })();");
+
+var statusCode = jsResult.Get("statusCode");
+if (statusCode.IsNumber())
+{
+    result.StatusCode = (int)statusCode.AsNumber();
+}
+
+var resultJson = JsonSerializer.Serialize(result);
+await File.WriteAllTextAsync("result.json", resultJson);
+
+return;
+
+class Result
+{
+    public int StatusCode { get; set; }
+    public Dictionary<string, string> Headers { get; set; }
+    public string Body { get; set; }
+    public string AdditionalLogMessage { get; set; }
+}
---
+++ src/Services/WebApp/WebApp.FunctionInvoker/data/handler.js
@@ -0,0 +1,8 @@
+function handler(request) {
+    return {
+        status: 200,
+        body: {
+            message: 'Hello from the function invoker!'
+        }
+    }
+}
