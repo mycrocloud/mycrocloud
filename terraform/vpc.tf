3501e1e0c04a84884197391151ca1eb817e146e4
Nam Pham <pvnam95@hotmail.com>
2025-10-02 16:41:45 +0900

    add subnet cidr_blocks

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,14 +5,17 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "az1" {
   vpc_id = aws_vpc.vpc.id
   availability_zone = "ap-northeast-1a"
+  cidr_block = "10.0.0.0/20"
 }
 
 resource "aws_subnet" "az2" {
   vpc_id = aws_vpc.vpc.id
   availability_zone = "ap-northeast-1c"
+  cidr_block = "10.0.16.0/20"
 }
 
 resource "aws_subnet" "az3" {
   vpc_id = aws_vpc.vpc.id
   availability_zone = "ap-northeast-1d"
+  cidr_block = "10.0.32.0/20"
 }
