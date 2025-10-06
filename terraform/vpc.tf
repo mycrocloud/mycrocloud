5b020c85fc311f731d55b36e673df2cccbfb5973
Nam Pham <pvnam95@hotmail.com>
2025-10-06 18:43:44 +0900

    add ig

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -19,3 +19,16 @@ resource "aws_subnet" "az3" {
   availability_zone = "ap-northeast-1d"
   cidr_block        = "10.0.32.0/20"
 }
+
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
+}
