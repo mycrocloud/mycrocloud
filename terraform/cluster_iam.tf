84b383b12c7e51b320c88ec88549511fd36e9052
Nam Pham <pvnam95@hotmail.com>
2025-10-06 17:03:40 +0900

    add output

--- terraform/cluster_iam.tf
+++ terraform/cluster_iam.tf
@@ -15,11 +15,6 @@ resource "aws_iam_role" "cluster_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSBlockStoragePolicy" {
-  role       = aws_iam_role.cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSBlockStoragePolicy"
-}
-
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   role       = aws_iam_role.cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,7 @@ output "profile" {
 output "cluster_name" {
   value = aws_eks_cluster.cluster.name
 }
+
+output "kubeconfi_command" {
+  value = "eksctl utils write-kubeconfig --region ${var.region} --cluster ${aws_eks_cluster.cluster.name} --profile ${var.profile}"
+}
