baaad6a563e670e7fb4e1d50dd88f3891cd5fde9
Nam Pham <pvnam95@hotmail.com>
2025-10-06 18:08:09 +0900

    add node groups

---
+++ terraform/node_group.tf
@@ -0,0 +1,47 @@
+resource "aws_iam_role" "node_group_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.node_group_role.arn
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.node_group_role.arn
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.node_group_role.arn
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_eks_node_group" "node_group" {
+  cluster_name = aws_eks_cluster.cluster.name
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 2
+  }
+
+  node_role_arn = aws_iam_role.node_group_role.arn
+  subnet_ids = [
+    aws_subnet.az1.id,
+    aws_subnet.az2.id,
+    aws_subnet.az3.id,
+  ]
+  instance_types = ["t3.small"]
+}
+
