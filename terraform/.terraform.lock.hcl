ecec16611a77905a98b823723d300d6c91c347cf
Nam Pham <pvnam95@hotmail.com>
2025-10-07 22:33:24 +0900

    add alb iam

--- terraform/.terraform.lock.hcl
+++ terraform/.terraform.lock.hcl
@@ -28,6 +28,7 @@ provider "registry.terraform.io/hashicorp/aws" {
 provider "registry.terraform.io/hashicorp/kubernetes" {
   version = "2.38.0"
   hashes = [
+    "h1:7nJdsd1RMPBtOjDXidB37+KSDN5VcOWkbkow69qJVGc=",
     "h1:soK8Lt0SZ6dB+HsypFRDzuX/npqlMU6M0fvyaR1yW0k=",
     "zh:0af928d776eb269b192dc0ea0f8a3f0f5ec117224cd644bdacdc682300f84ba0",
     "zh:1be998e67206f7cfc4ffe77c01a09ac91ce725de0abaec9030b22c0a832af44f",
--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,16 +1,29 @@
 
-resource "aws_iam_openid_connect_provider" "default" {
+resource "aws_iam_openid_connect_provider" "oidc_provider" {
   client_id_list = ["sts.amazonaws.com"]
   url = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
 
+resource "aws_iam_role" "alb_role" {
+  assume_role_policy = templatefile("alb_trust_policy.json.tftpl", {
+    oidc_provider_arn = aws_iam_openid_connect_provider.oidc_provider.arn,
+    oidc_provider_url = replace(aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")
+  })
+}
+
 resource "aws_iam_policy" "AWSLoadBalancerControllerIAMPolicy" {
+  name = "AWSLoadBalancerControllerIAMPolicy"
   policy = file("iam_policy.json")
 }
 
-resource "kubernetes_service_account" "alb" {
-  metadata {
-    name = "aws-load-balancer-controller"
-    namespace = "kube-system"
-  }
+resource "aws_iam_role_policy_attachment" "alb_role_attachment" {
+  role       = aws_iam_role.alb_role.name
+  policy_arn = aws_iam_policy.AWSLoadBalancerControllerIAMPolicy.arn
 }
+
+# resource "kubernetes_service_account" "alb" {
+#   metadata {
+#     name = "aws-load-balancer-controller"
+#     namespace = "kube-system"
+#   }
+# }
