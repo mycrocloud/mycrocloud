05bf16581c8ef6627ccfd275033da44569359630
Nam Pham <pvnam95@hotmail.com>
2025-10-09 10:41:30 +0900

    format cloudflare_dns_record resource for consistency

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -7,10 +7,10 @@ resource "cloudflare_zone" "domain" {
 
 resource "cloudflare_dns_record" "root" {
   zone_id = var.cloudflare_zone_id
-  name = "@"
-  type = "CNAME"
-  ttl = 1
+  name    = "@"
+  type    = "CNAME"
+  ttl     = 1
   content = data.kubernetes_service.nginx_ingress_lb.status[0].load_balancer[0].ingress[0].ip
-  
+
   proxied = true
 }
