9ee5afd7be641a5b262e957ce7c7b741bfc10f9e
Nam Pham <pvnam95@hotmail.com>
2025-10-15 14:09:28 +0900

    simple infra

---
+++ deploy/simple/terraform/terraform.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] // Canonical
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_key_pair" "ssh_key" {
+  public_key = var.ssh_public_key
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "ig" {
+
+}
+
+resource "aws_internet_gateway_attachment" "ig_vpc" {
+  internet_gateway_id = aws_internet_gateway.ig.id
+  vpc_id              = aws_vpc.vpc.id
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.subnet.id
+}
+
+
+resource "aws_instance" "server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  key_name                    = aws_key_pair.ssh_key.key_name
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.subnet.id
+}
