7c834adbd68b1f7ce8a8bbaa09d10955d7cb9d15
Nam Pham <pvnam95@hotmail.com>
2025-10-27 12:10:12 +0900

    add outputs

--- deploy/simple/infra/modules/auth0/outputs.tf
+++ deploy/simple/infra/modules/auth0/outputs.tf
@@ -1,3 +1,11 @@
+output "domain" {
+  value = var.domain
+}
+
 output "web_client_id" {
   value = auth0_client.web.client_id
+}
+
+output "api_identifier" {
+  value = auth0_resource_server.api.identifier
 }
--- deploy/simple/infra/outputs.tf
+++ deploy/simple/infra/outputs.tf
@@ -1,3 +1,4 @@
+# Server
 output "instance_ip" {
   value = aws_instance.server.public_ip
 }
@@ -6,10 +7,20 @@ output "instance_user" {
   value = "ubuntu"
 }
 
+# DNS
 output "host" {
   value = data.cloudflare_zone.zone.name
 }
 
-output "ssh_command" {
-  value = "ssh ubuntu@${aws_instance.server.public_ip} -i <key>"
+# Identity
+output "auth0_domain" {
+  value = module.auth0.domain
+}
+
+output "auth0_web_client_id" {
+  value = module.auth0.web_client_id
+}
+
+output "auth0_api_identifier" {
+  value = module.auth0.api_identifier
 }
