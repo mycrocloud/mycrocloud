7a016d729fb2d738a10413e6935fa899cf63d471
Nam Pham <pvnam95@hotmail.com>
2025-10-29 23:18:54 +0900

    Connect Slack button

--- src/Web/src/pages/settings/index.tsx
+++ src/Web/src/pages/settings/index.tsx
@@ -2,9 +2,14 @@ import Connections from "./Connections";
 import Tokens from "./Tokens";
 
 export default function Settings() {
+  const slack =
+    "https://slack.com/oauth/v2/authorize?client_id=8036520278803.9790206079221&scope=chat:write,commands,channels:read,users:read&redirect_uri=https://mycrocloud.info/integrations/slack/oauth/callback";
   return (
     <div className="">
       <Connections />
+      <section>
+        <a href={slack}>Connect Slack</a>
+      </section>
       <Tokens />
     </div>
   );
