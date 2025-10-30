180817c582f883b71574636541c8d19649dfb6d5
Nam Pham <pvnam95@hotmail.com>
2025-10-30 11:32:42 +0900

    vite https

--- src/Web/vite.config.ts
+++ src/Web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -8,6 +9,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
+      mkcert(),
       {
         name: "inject-config-tag",
         apply: "build",
