c1a9c16d215cc887e86858acd8b008df3f9287c2
Nam Pham <pvnam95@hotmail.com>
2025-12-29 22:26:00 +0900

    src/pages/apps/Create.tsx

--- src/Web/src/pages/apps/Create.tsx
+++ src/Web/src/pages/apps/Create.tsx
@@ -3,6 +3,7 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button, HelperText, Label, Textarea, TextInput } from "flowbite-react";
 
 type Inputs = {
   name: string;
@@ -42,52 +43,33 @@ function AppCreate() {
     <form className="mx-auto mt-5 max-w-4xl" onSubmit={handleSubmit(onSubmit)}>
       <h1 className="font-semibold">Create app</h1>
       <div className="mb-5 mt-3">
-        <label htmlFor="name" className="mb-2 block  font-medium text-gray-900">
-          Name
-        </label>
-        <input
-          type="text"
-          id="name"
+        <Label>Name</Label>
+        <TextInput
           {...register("name")}
-          className="block w-full border border-gray-300 bg-gray-50 p-1.5  text-gray-900 focus:border-blue-500 focus:ring-blue-500 "
         />
-        {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+        {errors.name && <HelperText>{errors.name.message}</HelperText>}
       </div>
       <div className="mb-5 mt-3">
-        <label
-          htmlFor="description"
-          className="mb-2 block  font-medium text-gray-900 "
-        >
-          Description
-        </label>
-        <textarea
-          id="description"
-          rows={4}
+        <Label>Description</Label>
+        <Textarea
           {...register("description")}
-          className="block w-full border border-gray-300 bg-gray-50  text-gray-900 focus:border-cyan-500 focus:ring-cyan-500 disabled:cursor-not-allowed disabled:opacity-50 "
-        ></textarea>
-        {errors.description && (
-          <p className="text-red-500">{errors.description.message}</p>
-        )}
+        />
+        {errors.description && <HelperText>{errors.description.message}</HelperText>}
       </div>
       <div className="flex">
-        <button
-          type="button"
-          className="group relative me-1 ms-auto flex items-center justify-center border border-gray-300 bg-white p-0.5 text-center font-medium text-gray-900 focus:z-10 focus:outline-none focus:ring-2 focus:ring-cyan-300 enabled:hover:bg-gray-100"
+        <Button
           onClick={() => navigate("/apps")}
+          outline
+          className="ms-auto"
         >
-          <span className="flex items-center rounded-md px-3 py-1.5  transition-all duration-200">
-            Cancel
-          </span>
-        </button>
-        <button
+          Cancel
+        </Button>
+        <Button
           type="submit"
-          className="group relative flex items-center justify-center border border-transparent bg-primary p-0.5 text-center font-medium text-white focus:z-10 focus:outline-none focus:ring-2 focus:ring-cyan-300 enabled:hover:bg-cyan-700"
+          className="ms-2"
         >
-          <span className="flex items-center rounded-md px-3 py-1.5  transition-all duration-200">
-            Save
-          </span>
-        </button>
+          Save
+        </Button>
       </div>
     </form>
   );
