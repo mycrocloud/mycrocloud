53ee1e71629ea9fd901c3e345671473c14a29136
Nam Pham <pvnam95@hotmail.com>
2026-02-10 12:03:16 +0900

    feat: make ArtifactId nullable and add Building status to DeploymentStatus enum

--- src/Services/WebApp/Api.Domain/Entities/SpaDeployment.cs
+++ src/Services/WebApp/Api.Domain/Entities/SpaDeployment.cs
@@ -2,6 +2,7 @@ namespace Api.Domain.Entities;
 
 public enum DeploymentStatus
 {
+    Building = 0,
     Pending = 1,
     Extracting = 2,
     Ready = 3,
@@ -13,7 +14,7 @@ public class SpaDeployment : Deployment
 {
     public Guid? BuildId { get; set; }
     public AppBuild? Build { get; set; }
-    public Guid ArtifactId { get; set; }
-    public Artifact Artifact { get; set; } = null!;
+    public Guid? ArtifactId { get; set; }
+    public Artifact? Artifact { get; set; }
 }
 
