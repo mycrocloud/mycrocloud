1296f591791bda130db0104d82175bd9986bb28c
Nam Pham <pvnam95@hotmail.com>
2026-02-14 00:19:00 +0900

    feat: add default values for public key, Cloudflare zone ID, and Auth0 configurations

--- deploy/infra/variables.tf
+++ deploy/infra/variables.tf
@@ -1,9 +1,11 @@
 variable "public_key" {
   type = string
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+PV+eGYi2awncbY+nyqdaKcev4MV5KTcdcgZyZM4NC nam@mycrocloud.info"
 }
 
 variable "cloudflare_zone_id" {
   type = string
+  default = "c719907867c6f32aa82a8258adbf337a"
 }
 
 variable "cloudflare_api_token" {
@@ -13,10 +15,12 @@ variable "cloudflare_api_token" {
 
 variable "auth0_domain" {
   type = string
+  default = "dev-vzxphouz.us.auth0.com" # custom domain won't work
 }
 
 variable "auth0_client_id" {
   type = string
+  default = "KN9Y1imMXFv7CO5jjdY2LTBEavFlP4fU"
 }
 
 variable "auth0_client_secret" {
@@ -26,10 +30,12 @@ variable "auth0_client_secret" {
 
 variable "web_origin" {
   type = list(string)
+  default = ["https://mycrocloud.info"]
 }
 
 variable "auth0_github_oauth_app_client_id" {
   type = string
+  default = "0b52124cb3636aa4e203"
 }
 
 variable "auth0_github_oauth_app_client_secret" {
@@ -39,6 +45,7 @@ variable "auth0_github_oauth_app_client_secret" {
 
 variable "auth0_google_oauth_app_client_id" {
   type = string
+  default = "895232793314-iuf6v44ihi4ar16ei5ssmm6j69tl1m48.apps.googleusercontent.com"
 }
 
 variable "auth0_google_oauth_app_client_secret" {
