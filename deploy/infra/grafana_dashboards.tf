b01f5a3a81bf12e5a5a98d4ceeb402e04a93d753
Nam Pham <pvnam95@hotmail.com>
2026-02-28 14:43:54 +0900

    Add API logs dashboard configuration and update Grafana data source for Loki

--- deploy/infra/grafana_dashboards.tf
+++ deploy/infra/grafana_dashboards.tf
@@ -7,6 +7,11 @@ data "grafana_data_source" "prometheus" {
   name     = "grafanacloud-${var.grafana_cloud_stack_slug}-prom"
 }
 
+data "grafana_data_source" "loki" {
+  provider = grafana.stack
+  name     = "grafanacloud-${var.grafana_cloud_stack_slug}-logs"
+}
+
 resource "grafana_folder" "mycrocloud" {
   provider = grafana.stack
   title    = "MycroCloud"
@@ -23,5 +28,15 @@ resource "grafana_dashboard" "node_exporter" {
     ),
     "$${ds_prometheus}",
     data.grafana_data_source.prometheus.uid
+  )
+}
+
+resource "grafana_dashboard" "api_logs" {
+  provider = grafana.stack
+  folder   = grafana_folder.mycrocloud.uid
+  config_json = replace(
+    file("${path.module}/dashboards/api-logs.json"),
+    "$${DS_LOKI}",
+    data.grafana_data_source.loki.uid
   )
 }
