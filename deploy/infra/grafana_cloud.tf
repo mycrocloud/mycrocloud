a108295076ac722f11fbdaf9415afaf92b195c53
Nam Pham <pvnam95@hotmail.com>
2026-02-28 09:26:01 +0900

    Update Alloy environment secret to include Loki URL endpoint

--- deploy/infra/grafana_cloud.tf
+++ deploy/infra/grafana_cloud.tf
@@ -56,7 +56,7 @@ resource "grafana_cloud_access_policy_token" "prometheus_metrics_write" {
 resource "aws_secretsmanager_secret_version" "alloy_env" {
   secret_id = aws_secretsmanager_secret.alloy_env.id
   secret_string = jsonencode({
-    GRAFANA_CLOUD_LOKI_URL      = data.grafana_cloud_stack.this.logs_url
+    GRAFANA_CLOUD_LOKI_URL      = "${data.grafana_cloud_stack.this.logs_url}/loki/api/v1/push"
     GRAFANA_CLOUD_LOKI_USERNAME = tostring(data.grafana_cloud_stack.this.logs_user_id)
     GRAFANA_CLOUD_API_KEY       = grafana_cloud_access_policy_token.alloy_logs_write.token
   })
