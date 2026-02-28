0ccd409f7d4c8ff5a34d7c298a21a01067c7c65f
Nam Pham <pvnam95@hotmail.com>
2026-02-28 09:04:33 +0900

    Add Prometheus metrics access policy and update configuration for secrets management

--- deploy/infra/grafana_cloud.tf
+++ deploy/infra/grafana_cloud.tf
@@ -33,11 +33,40 @@ resource "grafana_cloud_access_policy_token" "alloy_logs_write" {
   access_policy_id = grafana_cloud_access_policy.alloy_logs_write.policy_id
 }
 
+resource "grafana_cloud_access_policy" "prometheus_metrics_write" {
+  provider = grafana.cloud
+  name     = "${local.project_name}-prometheus-metrics-write"
+  region   = grafana_cloud_stack.this.region_slug
+
+  scopes = ["metrics:write"]
+
+  realm {
+    type       = "stack"
+    identifier = grafana_cloud_stack.this.id
+  }
+}
+
+resource "grafana_cloud_access_policy_token" "prometheus_metrics_write" {
+  provider         = grafana.cloud
+  name             = "${local.project_name}-prometheus-metrics-write-token"
+  region           = grafana_cloud_stack.this.region_slug
+  access_policy_id = grafana_cloud_access_policy.prometheus_metrics_write.policy_id
+}
+
 resource "aws_secretsmanager_secret_version" "alloy_env" {
   secret_id = aws_secretsmanager_secret.alloy_env.id
   secret_string = jsonencode({
     GRAFANA_CLOUD_LOKI_URL      = data.grafana_cloud_stack.this.logs_url
     GRAFANA_CLOUD_LOKI_USERNAME = tostring(data.grafana_cloud_stack.this.logs_user_id)
     GRAFANA_CLOUD_API_KEY       = grafana_cloud_access_policy_token.alloy_logs_write.token
+  })
+}
+
+resource "aws_secretsmanager_secret_version" "prometheus_yml" {
+  secret_id = aws_secretsmanager_secret.monitoring_prometheus_prometheus_yml.id
+  secret_string = jsonencode({
+    PROMETHEUS_URL      = data.grafana_cloud_stack.this.prometheus_url
+    PROMETHEUS_USERNAME = tostring(data.grafana_cloud_stack.this.prometheus_user_id)
+    GRAFANA_API_KEY     = grafana_cloud_access_policy_token.prometheus_metrics_write.token
   })
 }
