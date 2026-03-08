ca25638804674101babecd340e730263624022c8
Nam Pham <pvnam95@hotmail.com>
2026-03-08 12:47:28 +0900

    Refactor secrets configuration to use local variables for project_id and improve organization

--- deploy/infra/secrets.tf
+++ deploy/infra/secrets.tf
@@ -1,4 +1,8 @@
 locals {
+  project_id = "9dd69c49-ce80-477e-a92c-b4060039719a"
+}
+
+locals {
   api_secrets = [
     "ConnectionStrings__DefaultConnection",
     "ConnectionStrings__PubSub",
@@ -10,22 +14,32 @@ locals {
     "Storage__S3__SecretKey",
     "gha-mycrocloud.private-key.pem",
   ]
+
+  dbmigrator_secrets = [
+    "ConnectionStrings__DefaultConnection"
+  ]
+
+  lb_secrets = [
+    "certs/mycrocloud.online.key",
+    "certs/mycrocloud.site.key"
+  ]
 }
 
 resource "bitwarden-secrets_secret" "api_secrets" {
   for_each   = toset(local.api_secrets)
   key        = "api/${each.value}"
-  project_id = "9dd69c49-ce80-477e-a92c-b4060039719a"
+  project_id = local.project_id
 }
 
-locals {
-  dbmigrator_secrets = [
-    "ConnectionStrings__DefaultConnection"
-  ]
-}
 
 resource "bitwarden-secrets_secret" "dbmigrator_secrets" {
   for_each   = toset(local.dbmigrator_secrets)
   key        = "dbmigrator/${each.value}"
-  project_id = "9dd69c49-ce80-477e-a92c-b4060039719a"
+  project_id = local.project_id
+}
+
+resource "bitwarden-secrets_secret" "lb_secrets" {
+  for_each   = toset(local.lb_secrets)
+  key        = "lb/${each.value}"
+  project_id = local.project_id
 }
